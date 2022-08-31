f50090ba33963e2809b20e931c0c5a3b4ad4d251
Dannie <dangrgen@gmail.com>
2022-08-31 14:47:08 +0600

    Добавьте файлы проекта.

---
+++ Models/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace MVC.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ContactPhone { get; set; }
+
+        public int PhoneId { get; set; }
+        public Phone Phone { get; set; }
+    }
+}
